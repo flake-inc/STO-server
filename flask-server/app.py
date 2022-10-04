6ab9e029c4b417d322748680f4a541f534bfe283
supulkalhara <supul.19@cse.mrt.ac.lk>
2022-10-04 11:59:59 +0530

    backend connected to the database

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,16 +1,45 @@
 
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+from pymongo import MongoClient
+from flask import jsonify
+import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
+
+client = MongoClient('localhost', 27017)
+db = client["SafeTakeOff"]
+sto = db["Weather"]
+# print("Read Here")
+# print(client.server_info())
+print(sto.find_one())
+
+
+@app.route('/summary', methods=['GET', 'OPTIONS'])
+def get_summary():
+    dict = sto.find_one()
+    dict.pop('_id')
+    return jsonify(dict)
+
+df = pd.read_csv('./weather_data.csv')
 
 # Main function
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def home():
-    return 'You are directed to the home page!'
+    # return 'You are directed to the home page!'
+    return redirect('http://127.0.0.1:5173/')
 
-@app.route('/about')
-def about():
-    return 'You are directed to the about  info page!'
+
+@app.route('/dataframe', methods=['GET'])
+def get_dataframe():
+    # return 'You are directed to the home page!'
+    with open("./weather_data.csv") as f:
+            file_content = f.read()
+
+    return file_content 
 
 if __name__ == '__main__':
     app.run()
