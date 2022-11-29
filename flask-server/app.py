e842eeb9a8660f806a1952037cb9ef9392b5f115
Supul Kalhara <supul9229kalhara@gmail.com>
2022-11-29 21:25:32 +0530

    new changes on backend functions

--- flask-server/app.py
+++ flask-server/app.py
@@ -10,18 +10,13 @@ from werkzeug.utils import secure_filename
 import logging
 
 
-from bson import json_util, ObjectId# from db import get_db
+from bson import json_util, ObjectId  # from db import get_db
 from datetime import datetime
 import json
-from flask_cors import CORS,cross_origin
+from flask_cors import CORS, cross_origin
 from flask_restful import Api
 from flask_session import Session
 import redis
-
-
-
-
-
 
 
 # from werkzeug.local import LocalProxy
@@ -30,10 +25,9 @@ import redis
 import db
 
 
-
 UPLOAD_FOLDER = 'datasets'
 ALLOWED_EXTENSIONS = set(['csv'])
-#test to insert data to the data base
+# test to insert data to the data base
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('HELLO WORLD')
 
@@ -47,13 +41,14 @@ class MongoJsonEncoder(JSONEncoder):
             return str(obj)
         return json_util.default(obj, json_util.CANONICAL_JSON_OPTIONS)
 
+
 app = Flask(__name__)
 # app.config.from_object(ApplicationConfig)
 
-CORS(app,supports_credentials=True)
+CORS(app, supports_credentials=True)
 
 
-server_session= Session(app)
+server_session = Session(app)
 app.config["secret_key"] = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = False
@@ -68,7 +63,7 @@ app.json_encoder = MongoJsonEncoder
 # @app.route('/upload', methods=['POST'])
 # def fileUpload():
 
-#     print(" dude")  
+#     print(" dude")
 #     print(os.path)
 #     target=UPLOAD_FOLDER
 #     if not os.path.isdir(target):
@@ -84,56 +79,53 @@ app.json_encoder = MongoJsonEncoder
 #     return response
 
 @app.route("/info")
-
 def Get_Current_User():
 
-    user_id=session.get('user_id')
-    print('hi',user_id)
+    user_id = session.get('user_id')
+    print('hi', user_id)
 
     if not user_id:
         return jsonify({'error': 'Unauthorized '}), 401
 
-
-    #Retrieve a user by email:
-    user= db.user.find_one({'username':user_id})
+    # Retrieve a user by email:
+    user = db.user.find_one({'username': user_id})
     return jsonify({
         "username": user.username
     })
 
-@app.route("/login", methods=["POST"])
 
+@app.route("/login", methods=["POST"])
 def User_Login():
 
     email = request.json['email']
     password = request.json["password"]
     print("hello", password)
-    #Retrieve a user by email:
-    user=db.user.find_one({'username':email})
-   
+    # Retrieve a user by email:
+    user = db.user.find_one({'username': email})
 
     # if user is already register show this error message
 
-    if user is None :
+    if user is None:
         return jsonify({'error': 'User error'}), 401
 
     if user['password'] != password:
         return jsonify({'error': 'Password error'}), 401
 
-    session["user_id"]=user['username']
+    session["user_id"] = user['username']
     print(session['user_id'])
-
 
     return jsonify({
         "email": user['username'],
         "Message": "you are Successfully Logged In"
     })
 
-@app.route("/logout", methods=["POST"])
 
+@app.route("/logout", methods=["POST"])
 def User_Logout():
     print(session)
     session.pop(session["user_id"])
     return "200"
+
 
 @app.route('/')
 def flask_mongodb_atlas():
@@ -148,10 +140,9 @@ def flask_mongodb_atlas():
     # # dict.pop('_id')
     # df = pd.json_normalize(d)
     # dftempyear = pd.DataFrame(df.groupby(['year'])['temperature'].mean())
-    
+
     # dftempyear['year'] = dftempyear.index
 
-    
     # x= dftempyear['year'].to_list()
     # y=dftempyear['temperature'].to_list()
     # result={}
@@ -160,47 +151,45 @@ def flask_mongodb_atlas():
 
     # # tempyear= dftempyear.to_json(orient='records')[1:-1].replace('},{', '} {')
     # # json_list = json.loads(json.dumps(list(dftempyear.T.to_dict().values())))
-    
+
     # # return jsonify(list(d)[0])
     # return jsonify(result)
 
-@app.route("/barchart",methods=['GET'])
-# @cross_origin(origin='*',headers=['Content- Type','Authorization'])
 
+@app.route("/barchart", methods=['GET'])
+# @cross_origin(origin='*',headers=['Content- Type','Authorization'])
 def barchart():
 
     d = db.minmaxdata.find({})
     df = pd.json_normalize(d)
     df = df.sort_values('monthdate')
 
-    result={}
+    result = {}
 
-    result['date']= df['monthdate'].to_list()
-    result['temp']= df['temperature'].to_list()  
-    result['wind']= df['wind_speed'].to_list()   
-    result['press']= df['mean_sea_level_pressure'].to_list()   
-    result['cloud']= df['total_cloud_cover'].to_list()   
-    result['mintemp']= df['mintemp'].to_list()   
-    result['minwind']= df['minwind'].to_list()   
-    result['minpress']= df['minpress'].to_list()   
-    result['mincloud']= df['mincloud'].to_list()   
-    result['maxtemp']= df['maxtemp'].to_list()   
-    result['maxwind']= df['maxwind'].to_list()   
-    result['maxpress']= df['maxpress'].to_list()   
-    result['maxcloud']= df['maxcloud'].to_list()  
+    result['date'] = df['monthdate'].to_list()
+    result['temp'] = df['temperature'].to_list()
+    result['wind'] = df['wind_speed'].to_list()
+    result['press'] = df['mean_sea_level_pressure'].to_list()
+    result['cloud'] = df['total_cloud_cover'].to_list()
+    result['mintemp'] = df['mintemp'].to_list()
+    result['minwind'] = df['minwind'].to_list()
+    result['minpress'] = df['minpress'].to_list()
+    result['mincloud'] = df['mincloud'].to_list()
+    result['maxtemp'] = df['maxtemp'].to_list()
+    result['maxwind'] = df['maxwind'].to_list()
+    result['maxpress'] = df['maxpress'].to_list()
+    result['maxcloud'] = df['maxcloud'].to_list()
 
-    return jsonify(result) 
+    return jsonify(result)
 
 
-@app.route("/getpred",methods=['GET'])
+@app.route("/getpred", methods=['GET'])
 # @cross_origin(origin='*',headers=['Content- Type','Authorization'])
-
 def getpred():
 
     today = datetime.today().strftime('%Y-%m-%d')
 
-
-    d = db.allpred.find({'date':today})
+    d = db.allpred.find({'date': today})
     df = pd.json_normalize(d)
     df = df.drop(['_id'], axis=1)
     df = df.sort_values('hour')
@@ -209,26 +198,24 @@ def getpred():
     # result={}
 
     # result['temp']= df['temperature'].to_list()
-    # result['date']= df['datestamp'].to_list() 
+    # result['date']= df['datestamp'].to_list()
     # result['hour']= df['hour'].to_list()
     # result['wind']= df['windspeed'].to_list()
     # result['press']= df['pressure'].to_list()
     # result['cloud']= df['cloudcover'].to_list()
 
-
     json_list = json.loads(json.dumps(list(df.T.to_dict().values())))
     return jsonify(json_list)
 
 
-@app.route("/test",methods=['GET'])
-
+@app.route("/test", methods=['GET'])
 def test():
 
     df = pd.read_csv('submission8.csv')
     x = json.loads(json.dumps(list(df.T.to_dict().values())))
 
     db.test.insert_many(x)
-    
+
     return x
 
 # @app.route("/login",methods=['GET'])
@@ -241,159 +228,159 @@ def test():
 #     x = json.loads(json.dumps(list(df.T.to_dict().values())))
 
 #     db.test.insert_many(x)
-    
+
 #     return x
 
-@app.route("/yearlyavg",methods=['GET'])
 
+@app.route("/yearlyavg", methods=['GET'])
 def yearlyavg():
 
     d = db.yearlyavg.find({})
     df = pd.json_normalize(d)
     df = df.sort_values('year')
 
-    result={}
+    result = {}
 
-    result['year']= df['year'].to_list()
-    result['temp']= df['temperature'].to_list()  
-    result['wind']= df['wind_speed'].to_list()   
-    result['press']= df['mean_sea_level_pressure'].to_list()   
-    result['cloud']= df['total_cloud_cover'].to_list()   
-   
-    return jsonify(result)  
+    result['year'] = df['year'].to_list()
+    result['temp'] = df['temperature'].to_list()
+    result['wind'] = df['wind_speed'].to_list()
+    result['press'] = df['mean_sea_level_pressure'].to_list()
+    result['cloud'] = df['total_cloud_cover'].to_list()
+
+    return jsonify(result)
 
 
-
-    
-@app.route("/monthlyavg",methods=['GET'])
-# @cross_origin(origin='*',headers=['Content- Type','Authorization'])
-
+@app.route("/monthlyavg", methods=['GET'])
 def monthlyavg():
 
     d = db.monthlyavg.find({})
     df = pd.json_normalize(d)
     df = df.sort_values('month')
 
-    result={}
+    result = {}
 
-    result['month']= df['month'].to_list()
-    result['temp']= df['temperature'].to_list()  
-    result['wind']= df['wind_speed'].to_list()   
-    result['press']= df['mean_sea_level_pressure'].to_list()   
-    result['cloud']= df['total_cloud_cover'].to_list()   
-   
-    return jsonify(result)  
+    result['month'] = df['month'].to_list()
+    result['temp'] = df['temperature'].to_list()
+    result['wind'] = df['wind_speed'].to_list()
+    result['press'] = df['mean_sea_level_pressure'].to_list()
+    result['cloud'] = df['total_cloud_cover'].to_list()
 
-@app.route("/aircraftsdata",methods=['GET'])
+    return jsonify(result)
+
+
+@app.route("/aircraftsdata", methods=['GET'])
 def aircraftsdata():
 
     d = db.AirCrafts.find({})
     df = pd.json_normalize(d)
-    # df = df.sort_values('Model')
 
-    result={}
+    result = {}
+    result['Model'] = df['Model'].to_list()
+    result['Temperature_t'] = df['Temperature Threshold'].to_list()
+    result['Wind_t'] = df['Wind Speed Threshold'].to_list()
+    result['Cloud_t'] = df['Total Cloud Cover Threshold'].to_list()
 
-    result['Model']= df['Model'].to_list()
-    # result['Make']= df['Make'].to_list()  
-    result['Year']= df['Year'].to_list()   
-    # result['Category']= df['Category'].to_list()   
-    result['Temperature_t']= df['Temperature Threshold'].to_list()   
-    result['Wind_t']= df['Wind Speed Threshold'].to_list()   
-    # result['Solar_t']= df['Solar Radiation Threshold'].to_list()   
-    result['Cloud_t']= df['Total Cloud Cover Threshold'].to_list()  
-    result['humidity_t']= df['Relative Humidity Threshold'].to_list()   
-    # result['thermal_t']= df['Thermal Radiation Threshold'].to_list()    
-
-    return jsonify(result)  
+    return jsonify(result)
 
 
-@app.route("/aircraftspie",methods=['GET'])
+@app.route("/aircraftspie", methods=['GET'])
 def aircraftspie():
 
-    d = db.AirCrafts.find({})
-    df = pd.json_normalize(d)
+    cat = db.AirCrafts.aggregate([
+        {"$group": {"_id": "$Category", "count": {"$sum": 1}}}
+    ])
+
+    make = db.AirCrafts.aggregate([
+        {"$group": {"_id": "$Make", "count": {"$sum": 1}}}
+    ])
+
+    cat_arr = []
+    cat_count_arr = []
+    make_arr = []
+    make_count_arr = []
+
+    for i in cat:
+        cat_arr.append(i['_id'])
+        cat_count_arr.append(i["count"])
+
+    for i in make:
+        make_arr.append(i['_id'])
+        make_count_arr.append(i["count"])
+
     result={}
+    result['Make']= make_arr
+    result['MakeCount']= make_count_arr
+    result['Categories'] = cat_arr
+    result['CategoryCount']= cat_count_arr
+    return jsonify(result)
 
-   
-    Categories = df.groupby("Category")
-    CategoryCount = df.groupby("Category")["Model"].count()
-    Make = df.groupby("Make")
-    MakeCount = df.groupby("Make")["Model"].count()
 
-    result['Model']= df['Model'].to_list()
-    result['Make']= list(Make.groups.keys())
-    result['MakeCount']= MakeCount.to_list()  
-    result['Categories'] = list(Categories.groups.keys())
-    result['CategoryCount']= CategoryCount.to_list()   
-
-    return jsonify(result)  
-
-@app.route("/aircraftscategories",methods=['GET'])
+@app.route("/aircraftscategories", methods=['GET'])
 def aircraftscategories():
 
-    d = db.AirCrafts.find({})
-    df = pd.json_normalize(d)
-    result={}
+    d = db.AirCrafts.distinct("Category")
+    result = {}
+    result['Categories'] = d
 
-    Categories = df.groupby("Category")
-    result['Categories'] = list(Categories.groups.keys())
+    return jsonify(result)
 
-    return jsonify(result)  
 
-@app.route("/temppred",methods=['GET'])
-
+@app.route("/temppred", methods=['GET'])
 def temppred():
-    d = db.TempPred.find({})
+    d = db.TempPred.find().limit(1000)
     df = pd.json_normalize(d)
     df = df.sort_values('ds')
-    result={}
+    result = {}
 
-    result['ds'] = df['ds'].to_list()[-100:]
-    result['yhat1']= df['yhat1'].to_list()[-100:]
-   
-    return jsonify(result)  
+    result['ds'] = df['ds'].to_list()
+    result['yhat1'] = df['yhat1'].to_list()
+
+    return jsonify(result)
 
 
-@app.route("/pressurepred",methods=['GET'])
+@app.route("/pressurepred", methods=['GET'])
 def pressurepred():
-    d = db.PressurePred.find({})
+    d = db.PressurePred.find().limit(1000)
     df = pd.json_normalize(d)
     df = df.sort_values('ds')
 
-    result={}
-    result['ds']= df['ds'].to_list()[-100:]
-    result['yhat1']= df['yhat1'].to_list()[-100:]  
-   
-    return jsonify(result) 
+    result = {}
+    result['ds'] = df['ds'].to_list()
+    result['yhat1'] = df['yhat1'].to_list()
 
-@app.route("/cloudpred",methods=['GET'])
+    return jsonify(result)
+
+
+@app.route("/cloudpred", methods=['GET'])
 def cloudpred():
-    d = db.CloudCoverPred.find({})
+    d = db.CloudCoverPred.find().limit(1000)
     df = pd.json_normalize(d)
     df = df.sort_values('ds')
 
-    result={}
-    result['ds']= df['ds'].to_list()[-100:]
-    result['yhat1']= df['yhat1'].to_list()[-100:]  
-   
-    return jsonify(result) 
+    result = {}
+    result['ds'] = df['ds'].to_list()
+    result['yhat1'] = df['yhat1'].to_list()
 
-@app.route("/windpred",methods=['GET'])
+    return jsonify(result)
+
+
+@app.route("/windpred", methods=['GET'])
 def windpred():
-    d = db.WindSpeedPred.find({})
+    d = db.WindSpeedPred.find().limit(1000)
     df = pd.json_normalize(d)
     df = df.sort_values('ds')
 
-    result={}
-    result['ds']= df['ds'].to_list()[-100:]
-    result['yhat1']= df['yhat1'].to_list()[-100:]  
-   
-    return jsonify(result) 
+    result = {}
+    result['ds'] = df['ds'].to_list()
+    result['yhat1'] = df['yhat1'].to_list()
 
-@app.route("/check_today_danger",methods=['GET'])
+    return jsonify(result)
+
+
+@app.route("/check_today_danger", methods=['GET'])
 def check_today_danger():
     dangered_aircrafts = []
-    model_list = []  
+    model_list = []
     make_list = []
     cat_list = []
     year_list = []
@@ -405,13 +392,13 @@ def check_today_danger():
     print("\n====================================")
     print("local time:", new_dt)
 
-    wind = db.WindSpeedPred.find_one({ "ds": new_dt })
+    wind = db.WindSpeedPred.find_one({"ds": new_dt})
     print("wind predict", wind['yhat1'])
 
-    temp = db.TempPred.find_one({ "ds": new_dt })
+    temp = db.TempPred.find_one({"ds": new_dt})
     print("temp predict", temp['yhat1'])
 
-    cloud = db.CloudCoverPred.find_one({ "ds": new_dt })
+    cloud = db.CloudCoverPred.find_one({"ds": new_dt})
     print("cloud cover predict", cloud['yhat1'])
 
     aircrafts = db.AirCrafts.find({})
@@ -445,19 +432,15 @@ def check_today_danger():
             REG_list.append(df_air['REG'][i])
             continue
 
+    result = {}
 
-    
-    result={}
-
-    result['Model']= model_list
-    result['Make']= make_list
-    result['Category']= cat_list    
-    result['Year']= make_list
-    result['REG']= cat_list
-    return jsonify(result)  
-
+    result['Model'] = model_list
+    result['Make'] = make_list
+    result['Category'] = cat_list
+    result['Year'] = make_list
+    result['REG'] = cat_list
+    return jsonify(result)
 
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=True)
-
+    app.run(port=5000, debug=True)
